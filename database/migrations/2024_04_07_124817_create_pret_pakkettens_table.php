f4f7f6cc6f31325a1d842b44d3b1a894d1b2e075
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-07 15:03:52 +0200

    een nieuwe migrations gemaakt en de seerder aangepast voor de fake data

--- database/migrations/2024_04_07_124817_create_pret_pakkettens_table.php
+++ database/migrations/2024_04_07_124817_create_pret_pakkettens_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('pret_pakkettens', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price');
             $table->timestamps();
         });
     }
